3345ff13a1dfc0c6c3e9c17442e3b4aec2980428
Phuong <trantuanphuong19941994@gmail.com>
2020-09-21 11:00:07 +0700

    Updates

---
+++ precache-manifest.18fc65c87a83f9964ce17c224e1ed8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a0b5dc58a875ed29eb4cf31fc54ba2a",
+    "url": "/react-ecommerce-app/index.html"
+  },
+  {
+    "revision": "adf3b1c46d7d1237c7e3",
+    "url": "/react-ecommerce-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bf203d67c8da08e913f9",
+    "url": "/react-ecommerce-app/static/js/2.d4f0f134.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ecommerce-app/static/js/2.d4f0f134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf3b1c46d7d1237c7e3",
+    "url": "/react-ecommerce-app/static/js/main.d977c962.chunk.js"
+  },
+  {
+    "revision": "57ce4d22f978a9ba91ea",
+    "url": "/react-ecommerce-app/static/js/runtime-main.c038ed71.js"
+  }
+]);
