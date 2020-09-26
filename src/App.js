79f7f6d6cbecf6a2aee2df6936a9f30236c9085a
Phuong <trantuanphuong19941994@gmail.com>
2020-09-26 09:46:23 +0700

    Redirect user to homepage if they were already signed in

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
-
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action';
 
-function App({ setCurrentUser, ...props }) {
+function App({ setCurrentUser, currentUser, ...props }) {
   useEffect(() => {
     const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -40,14 +38,24 @@ function App({ setCurrentUser, ...props }) {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
-        <Route path="/signin" component={SignInAndSignUpPage} />
+        <Route
+          exact
+          path="/signin"
+          render={() =>
+            currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+          }
+        />
       </Switch>
     </div>
   );
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
